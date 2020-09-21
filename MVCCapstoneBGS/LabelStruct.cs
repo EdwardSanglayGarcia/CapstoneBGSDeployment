bf288d6171d619cdb380f8a06e3683b1019d3b82
NakagawaKanon <NakagawaKanon@users.noreply.github.com>
2020-09-22 02:42:04 +0800

    First
    First

---
+++ MVCCapstoneBGS/LabelStruct.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCapstoneBGS
+{
+    public struct LabelStruct
+    {
+
+        //LIST OF MESSAGES
+        public struct Administrator_Message
+        {
+            
+        }
+
+        public struct CommunityUser_Message
+        {
+            
+        }
+
+        //LIST OF TITLE TAGS
+        public struct Administrator
+        {
+            public const string AdministratorHomepage = "Administrator";
+            public const string Submitted = "Submitted";
+            public const string Accepted = "Accepted";
+            public const string InProgress = "In Progress";
+            public const string Completed = "Completed";
+            public const string Rejected = "Rejected";
+            public const string MonthlyReports = "Monthly Reports";
+            public const string YearlyReports = "Yearly Reports";
+            public const string Twitter = "Twitter";
+            public const string Volunteers = "Volunteers";
+        }
+
+        public struct CommunityUser
+        {
+            public const string CommunityUserHomepage = "Community User";
+            public const string SubmitReport = "Submit Report";
+            public const string ViewStatus = "View Status";
+            public const string Achievements = "Achievements";
+        }
+
+        public struct Others
+        {
+            public const string Leaderboards = "Leaderboards";
+        }
+
+
+    }
+}
