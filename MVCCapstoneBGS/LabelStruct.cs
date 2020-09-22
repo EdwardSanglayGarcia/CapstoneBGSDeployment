20312ec099946cb8978321e89bb7dd5a6e3c93ce
NakagawaKanon <NakagawaKanon@users.noreply.github.com>
2020-09-22 08:54:39 +0800

    MAHHJOR

--- MVCCapstoneBGS/LabelStruct.cs
+++ MVCCapstoneBGS/LabelStruct.cs
@@ -11,12 +11,24 @@ namespace MVCCapstoneBGS
         //LIST OF MESSAGES
         public struct Administrator_Message
         {
-            
+            public const string M_AdministratorHomepage = "";
+            public const string M_Submitted = "Submitted";
+            public const string M_Accepted = "Accepted";
+            public const string M_InProgress = "In Progress";
+            public const string M_Completed = "Completed";
+            public const string M_Rejected = "Rejected";
+            public const string M_MonthlyReports = "Monthly Reports";
+            public const string M_YearlyReports = "Yearly Reports";
+            public const string M_Twitter = "Twitter";
+            public const string M_Volunteers = "Volunteers";
         }
 
         public struct CommunityUser_Message
         {
-            
+            public const string M_CommunityUserHomepage = "Community User";
+            public const string M_SubmitReport = "Submit Report";
+            public const string M_ViewStatus = "View Status";
+            public const string M_Achievements = "Achievements";
         }
 
         //LIST OF TITLE TAGS
