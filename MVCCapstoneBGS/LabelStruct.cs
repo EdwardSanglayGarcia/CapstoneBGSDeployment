aef84a14c59550b5eacb5904da2bbd5b76204779
NakagawaKanon <NakagawaKanon@users.noreply.github.com>
2020-09-24 14:32:26 +0800

    3

--- MVCCapstoneBGS/LabelStruct.cs
+++ MVCCapstoneBGS/LabelStruct.cs
@@ -7,10 +7,24 @@ namespace MVCCapstoneBGS
 {
     public struct LabelStruct
     {
-
+        
         public struct Badges
         {
             public const string overallTop_1 = "https://i.imgur.com/M2yg9DO.png";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
+            //public const string EarthBender = "";
         }
 
         //LIST OF MESSAGES
